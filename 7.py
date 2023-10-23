14334b9ab43177aed50e0129fff7d66c0bdc25d0
DaryaVyalkova <148586142+DaryaVyalkova@users.noreply.github.com>
2023-10-24 03:06:57 +0700

    27.10.2023

---
+++ 7.py
@@ -0,0 +1,6 @@
+n = int(input('Введите число n: '))
+result = 1
+while n!=0:
+    result *= n
+    n -= 1
+print (result)
---
+++ 8.py
@@ -0,0 +1,7 @@
+n = int(input('Введите число n: '))
+while n>0:
+    if n%7==0:
+        print (n)
+    else:
+        n -=1
+    n-=1
