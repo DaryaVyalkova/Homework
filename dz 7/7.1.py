7da83352fb2585bbaeaec6ad856f8fce93045b74
DaryaVyalkova <dash.ka.05@yandex.ru>
2023-11-24 22:11:36 +0300

    24.11.2023

--- dz 7/7.1.py
+++ dz 7/7.1.py
@@ -1,4 +1,9 @@
-s1 = [1, 2, 3, 4, 5]
-s2 = [1, 2, 3, 4, 5]
-mysum = list(map(lambda s1, s2: s1+s2, s1, s2))
-print (mysum)
+s1 = [1, 2, 3, 4]
+s2 = [5, 6, 7, 8, 9]
+
+if len(s1) > len(s2):
+    s1, s2 = s2, s1
+mysum = list(map(lambda x, y: x + y, s1, s2[:len(s1)]))
+mysum.extend(s2[len(s1):])
+
+print(mysum)
